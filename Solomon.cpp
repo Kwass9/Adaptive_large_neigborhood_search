8d03449a4472fe2e5be9829f6a98d4c88f5ad07d
Kwass9 <maslik1@stud.uniza.sk>
2023-10-07 12:03:27 +0200

    Fix escape charakterov v ramci file path (subor nacitany)

--- Solomon.cpp
+++ Solomon.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cstring>
+#include <algorithm>
+
+void removeCharsFromString( std::string &str, char* charsToRemove ) {
+    for (unsigned int i = 0; i < strlen(charsToRemove); ++i) {
+        str.erase(std::remove(str.begin(),str.end(),charsToRemove[i]),str.end());
+    }
+}
 
 int main(int argc, char * argv[]) {
+    //argumenty
     std::string path;
+    double alfa1;
+    double alfa2;
+    double lambda;
+    double q;
+
     if (argc == 6) {
         path = argv[1];
+        removeCharsFromString(path,"\"");
         if (path.empty()) {
             std::cerr << "Not a valid argument" << std::endl;
         }
-
         if (atof(argv[2]) >= 0 && atof(argv[3]) >= 0) {
-            double lambda = atof(argv[2]);
-            double q = atof(argv[3]);
+            lambda = atof(argv[2]);
+            q = atof(argv[3]);
         } else {
             std::cerr << "We need non negative value for argument lambda" << std::endl;
         }
-        double alfa1 = atof(argv[4]);
-        double alfa2 = atof(argv[5]);
+        alfa1 = atof(argv[4]);
+        alfa2 = atof(argv[5]);
         double check = 0.00001; // pre pripad ze by si pocitac zmyslel podat desatinnu ciarku
-        if (alfa1 + alfa2 <= 1 + check || alfa1 + alfa2 >= 1 - check) {
+        if (alfa1 + alfa2 >= 1 + check || alfa1 + alfa2 <= 1 - check) {
             std::cerr << "Not valid value of alfas" << std::endl;
         }
     } else {
@@ -41,5 +55,10 @@ int main(int argc, char * argv[]) {
         std::cerr << "Subor nebol otvoreny" << std::endl;
     }
     input.close();
+
+    //kontrolny vypis
+    for (auto & i : data) {
+        std::cout << i << std::endl;
+    }
 
 }
